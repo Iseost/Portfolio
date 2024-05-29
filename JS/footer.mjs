9b12fc127c27a108ea5254f28a185ed27097ead1
Iseost <iselin.osterhus@gmail.com>
2024-05-29 20:40:58 +0200

    header and footer update
    Made header and footer in js and added it to the header and footer

---
+++ JS/footer.mjs
@@ -0,0 +1,5 @@
+const appFooter = `
+<img class="logo" src="../Image/logo iselin.png" alt="Logo" />
+<p>&copy; Iselin Engen</p>`;
+document.getElementById("appFooter").innerHTML = appFooter;
+
---
+++ JS/header.mjs
@@ -0,0 +1,9 @@
+const appHeader = `
+<a href="#"><img class="logo" src="../Image/logo iselin.png" alt="Logo" /></a>
+<div class="link_header flex">
+  <a href="./project.html">My projects</a>
+  <a href="www.linkedin.com/in/iselin-østerhus-engen-767b46284"
+    >Linkedin</a
+  >
+</div>`;
+document.getElementById("appHeader").innerHTML = appHeader;
